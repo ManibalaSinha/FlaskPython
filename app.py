d7c874d1e3f6358997fd3d3e27b51234832cd15d
Manibala Sinha <manibalak@gmail.com>
2025-06-19 12:19:18 -0400

    CRUD operation added for students

--- app.py
+++ app.py
@@ -1,102 +1,115 @@
-from flask import Flask, jsonify, request #give code access to third party libraries
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
+from flask_marshmallow import Marshmallow
 
-app = Flask(__name__) #set to Flask Constructor, will take name from script
+# App setup
+app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + 'C:\\Users\\manib\\FlaskPython\\students.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'students.db')
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
+# Models
+class Student(db.Model):
+    __tablename__ = 'students'
+    student_id = db.Column(Integer, primary_key=True)
+    student_name = db.Column(String)
+    student_class = db.Column(Integer)
+    student_subject = db.Column(String)
+    student_interest = db.Column(String)
+    student_grade = db.Column(Float)
+    student_city = db.Column(String)
+
+class StudentSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Student
+        load_instance = True
+
+student_schema = StudentSchema()
+students_schema = StudentSchema(many=True)
+
+# CLI Commands
 @app.cli.command('db_create')
 def db_create():
-   db.create_all()
-   print('Database created!')
-
+    db.create_all()
+    print("Database created!")
 
 @app.cli.command('db_drop')
 def db_drop():
-   db.drop_all()
-   print('Database dropped!')
-
+    db.drop_all()
+    print("Database dropped!")
 
 @app.cli.command('db_seed')
 def db_seed():
-   america = Student(student_name='America', student_class =5, student_subject ='python', student_interest ='Flask', student_grade=1.1, student_city= 'cty')
-   africa = Student(student_name='Africa', student_class =7, student_subject ='plugin', student_interest ='api', student_grade=2.1, student_city= 'ity')
-   australia = Student(student_name='Australia', student_class =9, student_subject ='django', student_interest ='rest', student_grade=1.3, student_city= 'cit')
+    s1 = Student(student_name="America", student_class=5, student_subject="Python", student_interest="Flask", student_grade=1.1, student_city="NYC")
+    s2 = Student(student_name="Africa", student_class=7, student_subject="Django", student_interest="API", student_grade=2.5, student_city="LA")
+    db.session.add_all([s1, s2])
+    db.session.commit()
+    print("Database seeded!")
 
-   db.session.add(america) 
-   db.session.add(africa)  
-   db.session.add(australia) 
+# Routes
+@app.route('/')
+def hello():
+    return "Hello from Kidz Learning Stations!"
 
-   test_user = User(first_name='Ram', last_name='Shayam', email='user@test.com', password='password')
+# --- CRUD ROUTES FOR STUDENT ---
 
-   db.session.add(test_user)
-   db.session.commit()
-   print('Database seeded!')
+# CREATE student
+@app.route('/students', methods=['POST'])
+def create_student():
+    data = request.get_json()
+    student = Student(
+        student_name=data.get('student_name'),
+        student_class=data.get('student_class'),
+        student_subject=data.get('student_subject'),
+        student_interest=data.get('student_interest'),
+        student_grade=data.get('student_grade'),
+        student_city=data.get('student_city')
+    )
+    db.session.add(student)
+    db.session.commit()
+    return student_schema.jsonify(student), 201
 
+# READ all students
+@app.route('/students', methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    return students_schema.jsonify(students), 200
 
-@app.route('/')# @ decorator gives special capabilities to function , define route of end point
-def hello_world():
-   return 'Hello Kidz Learning Stations!'
+# READ single student by ID
+@app.route('/students/<int:student_id>', methods=['GET'])
+def get_student(student_id):
+    student = Student.query.get_or_404(student_id)
+    return student_schema.jsonify(student), 200
 
+# UPDATE student
+@app.route('/students/<int:student_id>', methods=['PUT'])
+def update_student(student_id):
+    student = Student.query.get_or_404(student_id)
+    data = request.get_json()
 
-@app.route('/shorts')
-def shorts():
-   return jsonify(message='Hello from the Kidz Learning Stations'), 200
+    student.student_name = data.get('student_name', student.student_name)
+    student.student_class = data.get('student_class', student.student_class)
+    student.student_subject = data.get('student_subject', student.student_subject)
+    student.student_interest = data.get('student_interest', student.student_interest)
+    student.student_grade = data.get('student_grade', student.student_grade)
+    student.student_city = data.get('student_city', student.student_city)
 
+    db.session.commit()
+    return student_schema.jsonify(student), 200
 
-@app.route('/not_found')
-def not_found():
-   return jsonify(message='That resource was not found'), 404
+# DELETE student
+@app.route('/students/<int:student_id>', methods=['DELETE'])
+def delete_student(student_id):
+    student = Student.query.get_or_404(student_id)
+    db.session.delete(student)
+    db.session.commit()
+    return jsonify(message="Student deleted successfully"), 200
 
-
-@app.route('/parameters')
-def parameters():
-   name = request.args.get('name') #give access to all url values
-   age = int(request.args.get('age'))
-   if age < 18:
-      return jsonify(message="Sorry " + name + ", you are not old enough."), 401
-   else:
-      return jsonify(message="Welcome " + name + ", you are old enough")
-
-
-@app.route('/url_variables/<string:name>/<int:age>')
-def url_variables(name: str, age: int):
-   if age < 18:
-      return jsonify(message="Sorry " + name + ", you are not old enough."), 401
-   else:
-      return jsonify(message="Welcome " + name + ", you are old enough!")
-   
-@app.route('/students', methods=['GET']) #respond to GET request, method we want to accept
-def students(): # want to get all data from databse students
-   students_list = Student.query.all()
-   result = students_schema.dump(students_list)
-   return jsonify(result.data)
-
-
-#database models
-class User(db.Model):
-   __tablename__ = 'users'
-   id = db.Column(Integer, primary_key=True)
-   first_name = db.Column(String)
-   last_name = db.Column(String)
-   email = db.Column(String, unique=True)
-   password = Column(String)
-
-
-class Student(db.Model):
-   __tablename__ = 'students'
-   student_id = Column(Integer, primary_key=True)
-   student_name = Column(String)
-   student_class = Column(Integer)
-   student_subject = Column(String)
-   student_interest = Column(String)
-   student_grade = Column(Float)
-   student_city = Column(String)
-
+# ------------------------
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
